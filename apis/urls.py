d50f9559c53560628b9ed27f6434e67ad574ec47
Phawit Pornwattanakul <me@helloyeew.dev>
2024-03-08 17:14:04 +0700

    Add API for generate random client using in client

--- apis/urls.py
+++ apis/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
+from apis.views import get_random_question
+
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='api_token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='api_token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path("random_question", get_random_question, name="api_get_random_question")
 ]
--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from apps.question import generate_question, FailedToGenerateQuestion
+
+logger = logging.getLogger(__name__)
+
+
+@api_view(['GET'])
+def get_random_question(request):
+    """
+    View for return a random question in JSON format, will also use user's preference if user is authenticated.
+    """
+    try:
+        if request.user.is_authenticated:
+            question = generate_question(target_user=request.user)
+        else:
+            question = generate_question()
+        return Response(question)
+    except FailedToGenerateQuestion as e:
+        logger.error(f'Failed to generate question for user {request.user} (FailedToGenerateQuestion) with error: {str(e)}')
+        logger.exception(e)
+        return Response({
+            'message': str(e)
+        }, status=400)
+    except Exception as e:
+        logger.error(f'Error when getting random question for user {request.user} with error: {str(e)}')
+        logger.exception(e)
+        return Response({
+            'message': 'Internal server error'
+        }, status=500)
