75f90e6e25cbd1a31fcd394369da9e1e2fbf3395
Phawit Pornwattanakul <me@helloyeew.dev>
2024-03-11 11:57:15 +0700

    Add URL to new view

--- apis/urls.py
+++ apis/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-from apis.views import get_random_question
+from apis.views import *
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='api_token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='api_token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
-    path("random_question", get_random_question, name="api_get_random_question")
+    path("random_question", get_random_question, name="api_get_random_question"),
+
+    path("game/start", start_new_game, name="api_start_new_game"),
+    path("game/question", get_new_question, name="api_get_new_question"),
+    path("game/answer", answer_question, name="api_answer_question")
 ]
